a471ae2b3e99684ac1fc1b0cff01604f159b7d1c
jamil666 <jamil666@rambler.ru>
2017-04-20 14:04:17 +0400

    Final version
    admin account:
    
    login - admin
    pass - 123456789F!

--- Inventory/admin.py
+++ Inventory/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Computer
+
+
+class ComputerAdmin(admin.ModelAdmin):
+    search_fields = ('hostname', 'model', 'description', 'serial', 'date')
+    list_display = ('hostname', 'model', 'description', 'serial', 'date')
+
+
+admin.site.register(Computer, ComputerAdmin)
