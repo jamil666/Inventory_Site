b1bf7b1e3f632c8e42e875059532dee6c9ec8191
jamil666 <jamil666@rambler.ru>
2017-04-20 13:49:03 +0400

    Final version

--- Inventory/models.py
+++ Inventory/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 class Computer(models.Model):
 
-    hostname = models.CharField(max_length=100, null=True)
+    hostname = models.CharField(max_length=100, null=True, unique=True)
     description = models.CharField(max_length=200, null=True)
     model = models.CharField(max_length=100, null=True)
-    serial = models.CharField(max_length=100, null=True)
+    serial = models.CharField(max_length=100, null=True, unique=True)
     date = models.CharField(max_length=100, null=True)
 
     def __str__(self):
--- Inventory/views.py
+++ Inventory/views.py
@@ -1,63 +1,71 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from Inventory.models import Computer
 from .forms import Inventory_Form
 from django.db.models import Q
 import datetime
 
-# Create your views here.
+# Main page
 
 def main(request):
-    CompDB = Computer.objects.all()
-    Form = Inventory_Form(request.POST)
+    CompDB = Computer.objects.all()                 # Get all objects from Database
 
     if request.method == "POST":
-        SearchField = request.POST.get('search')
-        try:
-            Search = Computer.objects.filter(Q(hostname__contains=SearchField) |
+        SearchField = request.POST.get('search')    # Get query from Search Field
+
+        Search = Computer.objects.filter(Q(hostname__contains=SearchField) |            # Find object by query
                                              Q(description__contains=SearchField) |
                                              Q(model__contains=SearchField) |
                                              Q(serial__contains=SearchField))
 
-            context = {"Form": Form, "CompDB": CompDB, "Search":  Search}
-            return render(request, 'main.html', context)
-        except:
-            context = {"Form": Form, "CompDB": CompDB, "Message": "Record not found!"}
-            return render(request, 'main.html', context)
+        context = {"CompDB": CompDB, "Search": Search}
+        return render(request, 'main.html', context)
 
-    context = {"Form": Form, "CompDB": CompDB}
+    context = {"CompDB": CompDB}
     return render(request, 'main.html', context)
 
+# Add new item to database
+
 def AddNew(request):
-    CompDB = Computer.objects.all()
-    date = datetime.datetime.now()
+    CompDB = Computer.objects.all()                     # Get all objects from Database
+    date = datetime.datetime.now()                      # Get current time
 
     Form = Inventory_Form(request.POST)
     context = {"Form": Form, "CompDB": CompDB}
 
     if request.method == 'POST':
-        Hostname = request.POST.get('hostname')
-        Description = request.POST.get('description')
-        Model = request.POST.get('model')
-        Serial = request.POST.get('serial')
+        try:
+            Hostname = request.POST.get('hostname')
+            Description = request.POST.get('description')
+            Model = request.POST.get('model')
+            Serial = request.POST.get('serial')
 
-        Database = Computer(hostname=Hostname, description=Description, model=Model, serial=Serial, date=date.strftime("%d %b %Y"))
-        Database.save()
+            # Save new object to database
 
-        return redirect(main)
+            Database = Computer(hostname=Hostname, description=Description, model=Model, serial=Serial, date=date.strftime("%d %b %Y"))
+            Database.save()
+            return redirect(main)                       # Redirect to main page after successful adding new computer
+
+        except:
+            context = {"Form": Form, "CompDB": CompDB,
+                       "Message": "Computer with Hostname '%s' or Serial '%s' already exist in database!" %(Hostname, Serial)}
+            return render(request, 'AddNew.html', context)
+
 
     return render(request, 'AddNew.html', context)
 
+# Edit records view
 
 def Edit(request):
-    CompDB = Computer.objects.all()
-    Form = Inventory_Form(request.POST)
+    CompDB = Computer.objects.all()                 # Get all objects from Database
+
     date = datetime.datetime.now()
 
-    id = request.GET.get("id")
-    edited_device = Computer.objects.get(id=id)
+    id = request.GET.get("id")                      # Get object ID from request
+    edited_device = Computer.objects.get(id=id)     # Find computer object from database by ID
 
     if request.method == "POST":
+
+        # Add new values to computer object. If no new values, old value should be used
 
         hostname = request.POST.get("hostname")
         description = request.POST.get("description")
@@ -68,12 +76,13 @@ def Edit(request):
 
         return redirect(main)
 
-    context = {"Form": Form, "CompDB": CompDB, "Edited_device": edited_device}
+    context = {"CompDB": CompDB, "Edited_device": edited_device}
     return render(request, 'Edit.html', context)
 
+# Delete item from database
 
 def Delete(request):
-    id = request.GET.get("id")
-    deleted_item = Computer.objects.get(id=id)
-    deleted_item.delete()
+    id = request.GET.get("id")                      # Get object ID from request
+    deleted_item = Computer.objects.get(id=id)      # Find computer object from database by ID
+    deleted_item.delete()                           # Delete selected object from database
     return redirect(main)
